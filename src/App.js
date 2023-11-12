867bd96cc0dcf0d3343961b257e1d51effea2649
butterfly-sarah <sarahmohsen761@gmail.com>
2023-11-12 22:50:53 +0200

    third5 commit

--- src/App.js
+++ src/App.js
@@ -2,21 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import BasicPagination from '../src/components/Pagination'
 import Search from './components/Search';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import React from 'react';
 import Cv1 from './Pages/Cv1'
 import Profile from './Pages/Profile';
+import Page from './Pages/Page';
 function App() {
   return (
     <>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Cv1/>}></Route>
+        <Route path="/" element={<Page/>}></Route>
+        <Route path="/cv1" element={<Cv1/>}></Route>
         <Route path="/profile" element={<Profile/>}></Route>
       </Routes>
     </BrowserRouter>
-    {/* <BasicPagination/>
-    <Search/> */}
+    
     </>
   );
 }
---
+++ src/Pages/Page.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useRef, useState } from 'react'
+import '../styles/Cv1style.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import { Link } from 'react-router-dom'
+
+export default function Page() {
+    return(
+    <div>
+        <Link to="/cv1">FIRST PAGE CV</Link>
+        <br/>
+        <Link to="/profile">SECOND PAGE PROFILE</Link>
+    </div>)
+}
